87d22150a54dbee79b61606d55cc103f597c9b74
Luca <luca.tiengo.1997@gmail.com>
2022-01-17 18:34:05 +0100

    Luca Tiengo

--- Tesi_Game33/Assets/Scripts/EmanueleMessina.cs
+++ Tesi_Game33/Assets/Scripts/EmanueleMessina.cs
@@ -7,7 +7,7 @@ public class EmanueleMessina : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Luca Tiengo");
     }
 
     // Update is called once per frame
