8e88287ba754e37aaf1ead72d3f29bee35869e81
Megio <supermegiobros@gmail.com>
2022-01-17 18:15:20 +0100

    Aggiunto file

---
+++ Tesi_Game33/Assets/Scripts/LucaMegioranza.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LucaMegioranza : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("il meme la burla");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
